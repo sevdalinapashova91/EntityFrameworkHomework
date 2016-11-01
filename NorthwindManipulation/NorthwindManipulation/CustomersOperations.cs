4c72b92e6c2c052155cb2d6616fd3e53d24525dd
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-11-01 23:29:46 +0200

    //DAO class with update, create and delete from customers of Northwind database

---
+++ NorthwindManipulation/NorthwindManipulation/CustomersOperations.cs
@@ -0,0 +1,62 @@
+﻿namespace NorthwindManipulation
+{
+    using System.Linq;
+   
+    using System;
+    using System.Data.Entity.Infrastructure;
+
+    public static class CustomersOperations
+    {
+        public static void CreateCustomer(Models.Customer customer)
+        {
+            using (NorthwindEntities northwindEntities = new NorthwindEntities())
+            {
+                Customer newCustomer = new Customer();
+                newCustomer.Address = customer.Address;
+                newCustomer.City = customer.City;
+                newCustomer.CompanyName = customer.CompanyName;
+                newCustomer.ContactName = customer.ContactName;
+                newCustomer.ContactTitle = customer.ContactTitle;
+                newCustomer.Country = customer.Country;
+                northwindEntities.Customers.Add(newCustomer);
+                northwindEntities.SaveChanges();
+            }
+        }
+
+        public static void DeleteCustomer(string id)
+        {
+            using (NorthwindEntities northwindEntities = new NorthwindEntities())
+            {
+                var customerToDelete = GetCustomerById(id, northwindEntities);
+                northwindEntities.Customers.Remove(customerToDelete);
+                northwindEntities.SaveChanges();
+            }
+        }
+
+        public static void ChangeCustomer(string customerId, Models.Customer customer)
+        {
+            using (NorthwindEntities northwindEntities = new NorthwindEntities())
+            {
+                var customerToUpdate = GetCustomerById(customerId, northwindEntities);
+                if (customerToUpdate != null)
+                {
+                    customerToUpdate.Address = customer.Address;
+                    customerToUpdate.City = customer.City;
+                    customerToUpdate.CompanyName = customer.CompanyName;
+                    customerToUpdate.ContactName = customer.ContactName;
+                    customerToUpdate.ContactTitle = customer.ContactTitle;
+                    customerToUpdate.Country = customer.Country;
+                    northwindEntities.SaveChanges();
+                }
+            }
+        }
+
+        private static Customer GetCustomerById(string id, NorthwindEntities northwindEntities)
+        {
+            return northwindEntities.Customers
+                    .Select(customer => customer)
+                    .Where(customer => customer.CustomerID == id)
+                    .FirstOrDefault();
+        }
+    }
+}
---
+++ NorthwindManipulation/NorthwindManipulation/Models/Customer.cs
@@ -0,0 +1,31 @@
+﻿namespace NorthwindManipulation.Models
+{
+    public class Customer
+    {
+        public Customer(string companyName, string contactName, string contactTitle, string address,
+            string city, string region, string postCode, string country, string phone, string fax)
+        {
+            this.CompanyName = companyName;
+            this.ContactName = contactName;
+            this.ContactTitle = ContactTitle;
+            this.Address = address;
+            this.City = city;
+            this.Region = region;
+            this.PostalCode = postCode;
+            this.Country = country;
+            this.Phone = phone;
+            this.Fax = fax;
+        }
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+    }
+}
