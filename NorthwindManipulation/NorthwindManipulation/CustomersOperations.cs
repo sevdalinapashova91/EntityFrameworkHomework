9e242965c4dae5540b56d3f16c9d563650ba00e9
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-11-02 17:55:35 +0200

    //tasks

--- NorthwindManipulation/NorthwindManipulation/CustomersOperations.cs
+++ NorthwindManipulation/NorthwindManipulation/CustomersOperations.cs
@@ -1,10 +1,11 @@
 ﻿namespace NorthwindManipulation
 {
     using System.Linq;
-   
+
     using System;
     using System.Data.Entity.Infrastructure;
-
+    using System.Collections;
+    using System.Collections.Generic;
     public static class CustomersOperations
     {
         public static void CreateCustomer(Models.Customer customer)
@@ -48,6 +49,76 @@
                     customerToUpdate.Country = customer.Country;
                     northwindEntities.SaveChanges();
                 }
+            }
+        }
+
+        public static IEnumerable<Customer> GetAllCustomersWithOrdersFrom1997AndShippedToChina()
+        {
+            IEnumerable<Customer> customers = null;
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                customers = entities.Customers
+                    .Select(c => c)
+                    .Where(c =>
+                        c.Orders
+                            .Select(o => o)
+                            .Where(o => o.OrderDate != null && o.OrderDate.Value.Year == 1997 && o.ShipCountry == "Canada") != null)
+                    .ToList();
+            }
+
+            return customers;
+        }
+
+        public static IEnumerable<Customer> GetAllCustomersWithOrdersFrom1997AndShippedToChinaThroughDbContext()
+        {
+            IEnumerable<Customer> result = null;
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                string customersQuery = @"Select * 
+                                            From Customers c JOIN
+                                                    Orders o ON c.CustomerID = o.CustomerID
+                                            Where Year(o.OrderDate) = 1997 AND o.ShipCountry = 'Canada';";
+                result = entities.Database.SqlQuery<Customer>(customersQuery);
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<Order> GetSalesBySpecificRegionAndPeriod(DateTime start, DateTime end, string region)
+        {
+            IEnumerable<Order> orders = null;
+            using (NorthwindEntities entities = new NorthwindEntities())
+            {
+                orders = entities.Orders.Join(
+                     entities.Employees,
+                     order => order.EmployeeID,
+                     employee => employee.EmployeeID,
+                     (order, employee) => new { Order = order, Employee = employee })
+                    .Where(x => x.Employee.Region == region)
+                    .Select(a => a.Order)
+                    .ToList();
+            }
+
+            return orders;
+        }
+
+        public static void ConcurrenChangesSave()
+        {
+            NorthwindEntities northwindEntity1 = new NorthwindEntities();
+            NorthwindEntities nortwindEntity2 = new NorthwindEntities();
+            var customer = northwindEntity1.Customers.FirstOrDefault(x => x.Country == "Canada");
+            var customer2 = nortwindEntity2.Customers.FirstOrDefault(x => x.Country == "Canada");
+            customer.CompanyName = "Test";
+            customer2.CompanyName = "Test2";
+            nortwindEntity2.SaveChanges();
+            northwindEntity1.SaveChanges();
+        }
+
+        public static void NorthwindTwinCreation()
+        {// Added a connection string in App.config
+            using (NorthwindEntities northwindEntity = new NorthwindEntities())
+            {
+                northwindEntity.Database.CreateIfNotExists();
             }
         }
 
